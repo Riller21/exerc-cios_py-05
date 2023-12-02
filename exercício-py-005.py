319bf3474594277350e4d8196ae9bf5f0c2f60f2
Riller Rodrigues <rillernonato@yahoo.com.br>
2023-12-01 21:02:53 -0300

    primeiro commit

---
+++ exercício-py-005.py
@@ -0,0 +1,61 @@
+#função para definir se o número é impoar ou par
+def func_imp_par():
+    if  resultado % 2 != 0:
+        return ("impar")
+    else:
+        return ("par")
+    
+#função para definir se o número é negativo ou positivo
+def func_neg_pos():
+    if resultado < 0:
+        return ("negativo")
+    else:
+        return ("positivo")
+    
+#função para definir se o número é decimal ou inteiro
+def func_decimal():
+    if resultado != int(resultado):
+        return ("decimal")
+    else:
+        return ('inteiro')
+    
+
+#funções para definir as operações selecionadas
+def soma(a, b):
+    return a + b
+
+def subtração(a, b):
+    return a - b
+
+def multilicação(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: divisão por zero."
+    
+#selecione os número
+a = float(input("Insira o primeiro número: "))
+b = float(input("Insira o segundo número: "))
+
+#selecione qual operação deseja fazer 
+operação = input("Insira a operação que deseja realizar(soma,subtração,divisão ou multilicação): ")
+
+if operação == "soma":
+    resultado = soma(a, b)
+    print(f"Resultado: {resultado}")
+elif operação == "subtração":
+    resultado = subtracao(a, b)
+    print(f"Resultado: {resultado}")
+elif operação == "multiplicação":
+    resultado = multiplicacao(a, b)
+    print(f"Resultado: {resultado}")
+elif operação == "divisão":
+    resultado = divisao(a, b)
+    print(f"Resultado: {resultado}")
+else:
+    print("Operação inválida. Por favor, escolha uma operação válida.")
+
+print("O número",resultado,"é", func_imp_par(),",", func_neg_pos(), "e",func_decimal(),"." )
